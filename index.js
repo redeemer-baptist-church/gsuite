30c25bb6d53b6645021636953beb7de7e746604b
Tyler Distad <tyler.distad@gmail.com>
2019-08-24 21:00:45 -0400

    Try out a ManagerFactory pattern

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 Object.assign(
   module.exports,
   require('./lib/client'),
+  require('./lib/manager-factory'),
   require('./lib/calendar-manager'),
   require('./lib/group-manager'),
   require('./lib/people-manager'),
